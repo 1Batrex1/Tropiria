0c7898c4239f8e5d6f3bb1edb9a193d1cc4e6b78
1Batrex1 <bartosz.gajda@protonmail.com>
2024-04-14 21:30:24 +0200

    ADD data.sql there is a simple data to database

---
+++ backend/src/main/resources/sql/data.sql
@@ -0,0 +1,20 @@
+
+insert into role (name) values ('ROLE_ADMIN');
+insert into account (login, password,is_active) values ('admin', '$2a$12$TxsSy9F39BTyMO6kjgVLg.NnyY0hgAhIdFWhgogb0Xnv/MkhHmE3G', true);
+insert into account_roles (account_id, roles_id) values (1, 1);
+insert into species (name) values ('Gekon orzęsiony');
+insert into morph (name)values ('Harlequin');
+insert into morph (name)values ('Dalmatian');
+insert into morph (name)values ('Brindle');
+insert into morph (name)values ('Pinstripe');
+insert into morph (name)values ('Flame');
+insert into morph (name)values ('Patternless');
+insert into morph (name)values ('Bicolor');
+insert into morph (name)values ('Tiger');
+insert into morph (name)values ('Halloween');
+insert into morph (name)values ('Bicolor Crested gecko');
+insert into morph (name)values ('Creamsicle');
+insert into morph (name)values ('Phantom');
+insert into morph (name)values ('Blonde');
+insert into morph (name)values ('Flame Crested gecko');
+insert into morph (name)values ('Lilly white');
