4c3332260234dc21525787b7b99eb1f89ccfd8c5
1Batrex1 <bartosz.gajda@protonmail.com>
2024-07-30 15:30:16 +0200

    FIX saveAnimal method in controller

--- backend/src/main/java/pl/tropiria/backend/animal/AnimalController.java
+++ backend/src/main/java/pl/tropiria/backend/animal/AnimalController.java
@@ -1,6 +1,9 @@
 package pl.tropiria.backend.animal;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.CacheControl;
@@ -17,7 +20,8 @@ import static pl.tropiria.backend.config.constants.EndpointConstant.ANIMALS;
 @RequestMapping(ANIMALS)
 public class AnimalController {
 
-    private final static String SUCCESSFUL_SAVE_ANIMAL = "animal saved";
+    private static final String SUCCESSFUL_SAVE_ANIMAL = "animal saved";
+
 
 
     private final AnimalService animalService;
@@ -30,14 +34,23 @@ public class AnimalController {
                 .body(animalService.getAnimals());
     }
 
+    @GetMapping("/for-sale")
+    public ResponseEntity<Page<Animal>> getAnimalsForSale(Pageable pageable) {
+        return ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.maxAge(8, TimeUnit.HOURS))
+                .body(animalService.getAnimalsForSale(pageable));
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<AnimalDto> getAnimal(@PathVariable long id) {
         return ResponseEntity.ok(animalService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<?> saveAnimal(@RequestParam("animal") AnimalDto animalDto, @RequestParam("photos") MultipartFile[] photos) {
-        animalService.saveAnimal(animalDto, photos);
+    public ResponseEntity<?> saveAnimal(@RequestPart("animal") String animalJson, @RequestPart("photos") MultipartFile[] photos) throws JsonProcessingException {
+
+        animalService.saveAnimal(animalJson, photos);
         return ResponseEntity.status(HttpStatus.CREATED).body(SUCCESSFUL_SAVE_ANIMAL);
     }
 
