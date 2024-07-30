819583e75686fd3b42e23a008c0babacb1d9bef1
1Batrex1 <bartosz.gajda@protonmail.com>
2024-07-30 15:30:57 +0200

    ADD function to find animals for sale

--- backend/src/main/java/pl/tropiria/backend/animal/AnimalRepository.java
+++ backend/src/main/java/pl/tropiria/backend/animal/AnimalRepository.java
@@ -1,6 +1,10 @@
 package pl.tropiria.backend.animal;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +13,7 @@ public interface AnimalRepository extends JpaRepository<Animal, Long> {
     Animal findById(long id);
 
     Animal findByName(String name);
+
+    @Query("SELECT a FROM Animal a WHERE a.animalForSale IS NOT NULL")
+    Page<Animal> findAllAnimalsForSale(Pageable pageable);
 }
