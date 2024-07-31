1df215482566a2448c8c435513bcb5eeb359de88
1Batrex1 <bartosz.gajda@protonmail.com>
2024-07-31 12:55:33 +0200

    UPDATE updated packages

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,9 @@ import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, provideHttpClient, withFetch} from "@angular/common/http";
 import {Interceptor} from "./security/interceptor";
+import {provideToastr} from "ngx-toastr";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),provideToastr(),provideAnimations()]
 };
