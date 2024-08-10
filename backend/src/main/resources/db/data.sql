93021458819edc37737a57dfab8b1455a50fb321
1Batrex1 <bartosz.gajda@protonmail.com>
2024-08-10 20:26:04 +0200

    Fix data.sql

--- backend/src/main/resources/db/data.sql
+++ backend/src/main/resources/db/data.sql
@@ -11,31 +11,32 @@ insert into morph (name) values ('Patternless');
 insert into morph (name) values ('Bicolor');
 insert into morph (name) values ('Tiger');
 insert into morph (name) values ('Halloween');
-insert into morph (name) values ('Bicolor Crested gecko');
+insert into morph (name) values ('Bicolor Crested');
 insert into morph (name) values ('Creamsicle');
 insert into morph (name) values ('Phantom');
 insert into morph (name) values ('Blonde');
-insert into morph (name) values ('Flame Crested gecko');
+insert into morph (name) values ('Flame Crested');
 insert into morph (name) values ('Lilly white');
 
 
 
 INSERT INTO animal (name, description, sex, date_of_birth, species_id) VALUES ('zuko', 'this is zuko', 1, '2020-01-01', 1);
-Insert Into animal_morphs (animal_id, morphs_id) values (1, 1);
-Insert Into animal_morphs (animal_id, morphs_id) values (1, 2);
-
-
-INSERT INTO photos (photo_name) values ('placeholder-1.png'), ('placeholder-2.png'), ('placeholder-3.png');
-
-
-INSERT INTO animal_photos (animal_id, photos_id) values (1, 1), (1, 2), (1, 3);
-
 
 INSERT INTO animal (name, description, sex, date_of_birth, species_id) values ('gekon-1', 'this is gekon-1', 2, '2020-01-02', 1);
 
 INSERT INTO animal (name, description, sex, date_of_birth, species_id) values ('gekon-2', 'this is gekon-2', 1, '2020-01-03', 1);
 
 INSERT INTO animal (name, description, sex, date_of_birth, species_id) values ('gekon-3', 'this is gekon-3', 2, '2020-01-04', 1);
+
+
+Insert Into animal_morphs (animal_id, morphs_id) values (1, 1);
+Insert Into animal_morphs (animal_id, morphs_id) values (1, 2);
+
+
+INSERT INTO photos (photo_name) values ('placeholder-1.png'), ('placeholder-2.png'), ('placeholder-3.png'), ('placeholder-4.png');
+
+
+INSERT INTO animal_photos (animal_id, photos_id) values (1, 1), (2, 2), (3, 3), (4, 4);
 
 
 INSERT INTO animal_for_sale (animal_id, price,reservation_status) values (2, 100, 'ZAREZEWOWANO');
@@ -56,8 +57,6 @@ INSERT INTO animal_for_sale_parents (animal_for_sale_animal_id, parents_id) valu
 INSERT INTO animal_for_sale_parents (animal_for_sale_animal_id, parents_id) values (3, 1);
 
 INSERT INTO animal_for_sale_parents (animal_for_sale_animal_id, parents_id) values (4, 1);
-
-insert into animal_photos (animal_id, photos_id) values (2, 1), (3, 2), (4, 3);
 
 
 
