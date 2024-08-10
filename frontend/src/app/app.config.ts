2aacbf43614bbadb4896ee59bc3ad8d8ff133870
1Batrex1 <bartosz.gajda@protonmail.com>
2024-08-10 20:28:29 +0200

    Add new providers for animations date and http client

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,18 +4,21 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
 import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
-import {interceptor} from "./security/interceptor";
 import {provideToastr} from "ngx-toastr";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {jwtInterceptor} from "./security/jwtInterceptor";
+import {csrfInterceptor} from "./security/csrfInterceptor";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch(),withInterceptors([interceptor])),
+    provideHttpClient(withFetch(),withInterceptors([csrfInterceptor,jwtInterceptor])),
     provideToastr(),
     provideAnimations(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideNativeDateAdapter()
   ]
 };
