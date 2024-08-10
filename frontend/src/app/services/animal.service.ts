a70a1e2f2d3c92383959b3ddc33329d1e31d4c05
1Batrex1 <bartosz.gajda@protonmail.com>
2024-08-10 20:34:36 +0200

    change add new functions to animal service

--- frontend/src/app/services/animal.service.ts
+++ frontend/src/app/services/animal.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Animal} from "../entities/animal";
 import {Observable} from "rxjs";
 import {environment} from "../../constants/environment";
+import {AuthService} from "./auth.service";
+import {BrowserStorageService} from "./browser-storage.service";
+import {response} from "express";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
 
-  constructor(private http: HttpClient) { }
 
+  constructor(private http: HttpClient, private adminService: AuthService, private localStorage: BrowserStorageService) {
+  }
+
+  getParents(): Observable<Animal[]> {
+    return this.http.get<Animal[]>(environment.path.animals + '/parents');
+  }
 
   getAnimalsForSale(): Observable<getAnimalsForSale> {
-    return this.http.get<getAnimalsForSale>(environment.path.animals+'/for-sale?size=3');
+    return this.http.get<getAnimalsForSale>(environment.path.animals + '/for-sale?size=4');
+  }
+
+  getAnimals(number: number, size: number): Observable<getAnimals> {
+    return this.http.get<getAnimals>(environment.path.animals + '?page=' + number + '&size=' + size);
+  }
+
+  deleteAnimal(id: number): Observable<any>  {
+    return this.http.delete(environment.path.animals + '/' + id);
   }
 }
 
@@ -28,4 +44,18 @@ interface getAnimalsForSale {
   numberOfElements: number;
   first: boolean;
   empty: boolean;
+}
+
+interface getAnimals {
+
+  content: Animal[];
+
+  totalElements: number;
+
+  totalPages: number;
+
+  size: number;
+
+  number: number;
+
 }
