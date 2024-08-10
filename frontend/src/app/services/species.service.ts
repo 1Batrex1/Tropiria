5556505e7572c97184a1d3036b3452a2e39a66ea
1Batrex1 <bartosz.gajda@protonmail.com>
2024-08-10 20:33:46 +0200

    add species service

---
+++ frontend/src/app/services/species.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../constants/environment";
+import {Observable} from "rxjs";
+import {Species} from "../entities/species";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpeciesService {
+
+  constructor(private http :HttpClient) { }
+
+
+  getSpecies(): Observable<Species[]> {
+    return this.http.get<Species[]>(environment.path.species);
+  }
+}
