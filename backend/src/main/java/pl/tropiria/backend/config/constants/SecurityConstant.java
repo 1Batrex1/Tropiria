5fafc7b1cada3d74c8d2f455a287f56c8f7781ac
1Batrex1 <bartosz.gajda@protonmail.com>
2024-10-10 23:49:02 +0200

    Remove comment

--- backend/src/main/java/pl/tropiria/backend/config/constants/SecurityConstant.java
+++ backend/src/main/java/pl/tropiria/backend/config/constants/SecurityConstant.java
@@ -12,7 +12,6 @@ public class SecurityConstant {
 
     public static final String JWT_HEADER = "Authorization";
 
-    //Your secret key
     public static final String JWT_SECRET = "";
 
     public static final String JWT_NAME = "JWT_TOKEN";
